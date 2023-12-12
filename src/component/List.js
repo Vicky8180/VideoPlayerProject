2b537e663635fcff878754d5f42ddf40dff8943f
Vicky8180 <vyadav99x1@gmail.com>
2023-12-12 20:44:03 +0530

    deploy

--- src/component/List.js
+++ src/component/List.js
@@ -1,38 +1,78 @@
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import "./Total.css";
+// import { useSelector } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+// import { SelectedVideo } from './action';
+// export default function List() {
+
+//   const listData = useSelector((state) => state.ListState);
+// const navigate=useNavigate()
+//   useEffect(() => {
+//     console.log(listData);
+//   }, [listData]);
+// const dispatch=useDispatch();
+//   const sendHandler = (item) => {
+//     // navigate('/videoplayer', { state: { data: item } });
+
+//     dispatch(SelectedVideo(item))
+//   };
+//   return (
+//     <>
+//       <div className='list-1'>
+    
+//         {listData.length > 0 ? 
+//           listData.map((item, index) => (
+//             <>
+//             <button  key={index} onClick={() => sendHandler(item.video_id)}> 
+//              <div key={index} className='list-2'>  
+//                <div className='imgdiv'>  <img  alt="kk"/></div>
+//               <div>{item.title}</div>
+//             </div>
+//             </button>
+//             </>
+          
+//           ))
+//           : <div>Loading</div>}
+//       </div>
+//     </>
+//   );
+// }
+
+
+import React, { useEffect } from 'react';
 import "./Total.css";
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { SelectedVideo } from './action';
-export default function List() {
 
+export default function List() {
   const listData = useSelector((state) => state.ListState);
-const navigate=useNavigate()
+  const dispatch = useDispatch();
+
+  // useEffect will run whenever listData changes
   useEffect(() => {
     console.log(listData);
   }, [listData]);
-const dispatch=useDispatch();
+  console.log(listData);
+
   const sendHandler = (item) => {
     // navigate('/videoplayer', { state: { data: item } });
-
-    dispatch(SelectedVideo(item))
+    dispatch(SelectedVideo(item));
   };
+
   return (
     <>
       <div className='list-1'>
-    
         {listData.length > 0 ? 
           listData.map((item, index) => (
-            <>
-            <button  key={index} onClick={() => sendHandler(item.video_id)}> 
-             <div key={index} className='list-2'>  
-               <div className='imgdiv'>  <img  alt="kk"/></div>
-              <div>{item.title}</div>
-            </div>
+            <button className='btnlist' key={index} onClick={() => sendHandler(item.video_id)}> 
+              <div key={index} className='list-2'>  
+                <div className='imgdiv'>  <img alt="kk"  src={item.thambnail}/></div>
+                <div>{item.title}</div>
+              </div>
             </button>
-            </>
-          
           ))
           : <div>Loading</div>}
       </div>
--- src/component/SearchVideo.js
+++ src/component/SearchVideo.js
@@ -3,9 +3,11 @@ import axios from "axios"
 import "./Total.css"
 import { useDispatch } from 'react-redux';
 import { ListState } from './action';
-import { compose } from 'redux';
 export default function SearchVideo() {
-    const api_key = 'AIzaSyB98ZF7NVMXnYR6F7qlgYX8U1eRp9HWHtY';
+    // const api_key = 'AIzaSyAqByub8RUO62_WnAk7yweQHJgWgGzJ-Ig';
+    
+    console.log(process.env.REACT_APP_api_key)
+    
     const max_results = 10;
   
 
@@ -17,41 +19,54 @@ const dispatch = useDispatch();
       const fetchData = async () => {
         try {
           const response = await axios.get(
-            `https://www.googleapis.com/youtube/v3/search?key=${api_key}&part=snippet&q=${search}&type=video&maxResults=${max_results}`
+            `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_api_key}&part=snippet&q=${search}&type=video&maxResults=${max_results}`
           );
   
           const data = response.data;
+          console.log(data)
           const updatedListData = data.items.map(item => ({
             video_id: item.id.videoId,
             title: item.snippet.title,
+            thambnail: item.snippet.thumbnails.default.url
+            ,
           }));
   
           setListData(updatedListData);
 
           console.log(ListData)
-          dispatch(ListState(updatedListData));
+       
+            dispatch(ListState(updatedListData));
+       
+         
         } catch (error) {
           console.error('Error fetching data:', error.message);
         }
       };
   
       fetchData();
-    }, [api_key, search, max_results]);
+    }, [ search, max_results]);
 
     const handleInputChange = (e) => {
         setSearch(e.target.value);
       };
 
       console.log(search)
+
+
+
   return (
    <>
 
     <div className='searchvideo-1'>
 
-   <div className='inputsearch'>  <input style={{ width: '500px' , height: '28px'}} value={search}    onChange={handleInputChange}placeholder='search here'/></div>
-   <div className='searchbutton'> <button>Serach</button></div>
+   <div className='inputsearch'>  <input className='input-real' value={search}    onChange={handleInputChange} placeholder='search here'/></div>
+   {/* <div className='searchbutton'> <button >Serach</button></div> */}
 
     </div>
    </>
   )
 }
+
+
+
+
